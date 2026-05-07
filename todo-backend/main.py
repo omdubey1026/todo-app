429a8dbe584139a6db57734115c27fa0b67f2a80
Om Dwivedi <omdubey1026@gmail.com>
2026-05-07 14:42:46 +0530

    fixed cors

--- todo-backend/main.py
+++ todo-backend/main.py
@@ -1,8 +1,16 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_middleware(
     CORSMiddleware,
