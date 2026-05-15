a1a3baabc768bb34836aa53dab19cbd720bdc91f
Om Dwivedi <omdubey1026@gmail.com>
2026-05-15 15:32:37 +0530

    fixed frontend

--- todo-frontend/app/app.vue
+++ todo-frontend/app/app.vue
@@ -45,15 +45,23 @@ onMounted(fetchTodos)
   <div style="padding:20px">
     <h1>Todo App</h1>
 
-    <input v-model="newTodo" placeholder="Enter task" />
-    <button @click="addTodo">Add</button>
+   <input v-model="newTodo" placeholder="Enter task" />
 
-    <ul>
-      <li v-for="todo in todos" :key="todo[0]">
-        {{ todo[1] }}
-        <button @click="editTodo(todo[0], todo[1])">✏️</button>
-        <button @click="deleteTodo(todo[0])">❌</button>
-      </li>
-    </ul>
+<button @click="addTodo">Add</button>
+
+<ul>
+  <li v-for="todo in todos" :key="todo[0]">
+    {{ todo[1] }}
+
+    <button @click="editTodo(todo[0], todo[1])">
+      ✏️
+    </button>
+
+    <button @click="deleteTodo(todo[0])">
+      ❌
+    </button>
+  </li>
+</ul>
+
   </div>
 </template>
