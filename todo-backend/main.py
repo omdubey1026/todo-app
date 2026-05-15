35bcd682c2d117b8b20f45159fc9100eb01fe4ea
Om Dwivedi <omdubey1026@gmail.com>
2026-05-15 16:17:19 +0530

    fixed complete feature

--- todo-backend/main.py
+++ todo-backend/main.py
@@ -1,16 +1,8 @@
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,18 +15,24 @@ app.add_middleware(
 conn = sqlite3.connect("todos.db", check_same_thread=False)
 cursor = conn.cursor()
 
-cursor.execute(""" CREATE TABLE IF NOT EXISTS todos (
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS todos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     task TEXT,
     completed INTEGER DEFAULT 0
 )
- """)
+""")
+
 conn.commit()
+
 try:
-    cursor.execute("ALTER TABLE todos ADD COLUMN completed INTEGER DEFAULT 0")
+    cursor.execute(
+        "ALTER TABLE todos ADD COLUMN completed INTEGER DEFAULT 0"
+    )
     conn.commit()
 except:
     pass
+
 
 @app.get("/")
 def home():
@@ -49,23 +47,54 @@ def get_todos():
 
 @app.post("/todos")
 def add_todo(item: str):
-    cursor.execute("INSERT INTO todos (task) VALUES (?)", (item,))
+    cursor.execute(
+        "INSERT INTO todos (task, completed) VALUES (?, ?)",
+        (item, 0)
+    )
     conn.commit()
+
     return {"message": "added"}
 
 
 @app.delete("/todos/{id}")
 def delete_todo(id: int):
-    cursor.execute("DELETE FROM todos WHERE id = ?", (id,))
+    cursor.execute(
+        "DELETE FROM todos WHERE id = ?",
+        (id,)
+    )
+
     conn.commit()
+
     return {"message": "deleted"}
+
+
+@app.put("/todos/{id}")
+def update_todo(id: int, item: str):
+    cursor.execute(
+        "UPDATE todos SET task = ? WHERE id = ?",
+        (item, id)
+    )
+
+    conn.commit()
+
+    return {"message": "updated"}
+
 
 @app.put("/todos/{id}/complete")
 def complete_todo(id: int):
     cursor.execute(
-        "UPDATE todos SET completed = NOT completed WHERE id = ?",
+        """
+        UPDATE todos
+        SET completed =
+        CASE
+            WHEN completed = 0 THEN 1
+            ELSE 0
+        END
+        WHERE id = ?
+        """,
         (id,)
     )
+
     conn.commit()
 
-    return {"message": "updated"}
+    return {"message": "completed updated"}
